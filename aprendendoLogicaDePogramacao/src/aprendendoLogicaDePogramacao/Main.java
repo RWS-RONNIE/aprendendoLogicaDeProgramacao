8428de05a4de46c4f0444862f3b00e6e3e2ea195
RWS-RONNIE <ronnie.willyams.rws@gmail.com>
2026-06-15 12:47:32 -0300

    Estudo de variavel Java.

---
+++ aprendendoLogicaDePogramacao/src/aprendendoLogicaDePogramacao/Main.java
@@ -0,0 +1,9 @@
+package aprendendoLogicaDePogramacao;
+
+public class Main {
+    public static void main(String[] args) {
+		
+    	int idade = 30;
+    	System.out.println("A idade é: "+idade);
+	}
+}
