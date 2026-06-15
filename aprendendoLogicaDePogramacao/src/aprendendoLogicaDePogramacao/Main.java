6e9479e80e040fc48c6d4c1f913163eff20b553a
RWS-RONNIE <ronnie.willyams.rws@gmail.com>
2026-06-15 15:57:41 -0300

    Estudo de variaveis em Java.

--- aprendendoLogicaDePogramacao/src/aprendendoLogicaDePogramacao/Main.java
+++ aprendendoLogicaDePogramacao/src/aprendendoLogicaDePogramacao/Main.java
@@ -3,12 +3,18 @@ package aprendendoLogicaDePogramacao;
 public class Main {
     public static void main(String[] args) {
 		
-    	/*Tipos de variaveis java*/
+    	/*Tipos de variaveis java
     	
     	int numero = 30;
     	String frase = "hello world";
     	boolean decisao = true;
     	char letra = 'r';
     	double salario = 2.5;
+    	*/
+    	
+    	/*Constantes em java*/
+    	
+    	final int vida = 10;
+    	System.out.println("A vida do meu jogador é: "+vida);
 	}
 }
